3a1a80b8f5f05797cb13f41f94e1445a0b8c6de8
terrynie <88@terrynie.com>
2017-05-05 11:59:02 +0800

    Initial Commit

---
+++ exchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  exchange
+//
+//  Created by Terry on 2017/5/5.
+//  Copyright © 2017年 Terry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ exchange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  exchange
+//
+//  Created by Terry on 2017/5/5.
+//  Copyright © 2017年 Terry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
